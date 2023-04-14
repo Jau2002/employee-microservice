dbf01be8b10ee5efff7b6f9218feeede550c5c70
Jau2002 <idamendjaume@gmail.com>
2023-04-14 15:21:01 -0500

    use configure variables enviroment proyect

---
+++ src/types.d.ts
@@ -0,0 +1,21 @@
+import type { IncomingMessage, Server, ServerResponse } from 'http';
+
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			PORT: string;
+			DB_USER: string;
+			DB_PASSWORD: string;
+			DB_HOST: string;
+			DB_PORT: string;
+			DB_NAME: string;
+		}
+	}
+}
+
+type Serve = Server<typeof IncomingMessage, typeof ServerResponse>;
+
+declare module '*.yaml' {
+	const value: any;
+	export default value;
+}
