2fad86ccf5dc37cfb54f5fd3568719eee70a3d5b
Jau2002 <idamendjaume@gmail.com>
2023-04-14 15:27:54 -0500

    add rout sear for code employee

--- src/controllers/employees.controller.ts
+++ src/controllers/employees.controller.ts
@@ -13,3 +13,17 @@ export async function findAllEmployees(): Promise<Employee[]> {
 	});
 	return allEmployees;
 }
+
+export async function findByCodeEmployee(
+	code: number
+): Promise<Employee | null> {
+	const employeeRepository: Repository<Employee> =
+		AppDataSource.getRepository(Employee);
+
+	const allEmployees: Employee | null = await employeeRepository.findOne({
+		where: {
+			code,
+		},
+	});
+	return allEmployees;
+}
--- src/routes/employees.route.ts
+++ src/routes/employees.route.ts
@@ -1,5 +1,8 @@
 import { Router, type Request, type Response } from 'express';
-import { findAllEmployees } from '../controllers/employees.controller';
+import {
+	findAllEmployees,
+	findByCodeEmployee,
+} from '../controllers/employees.controller';
 import type Employee from '../entity/Employee';
 
 const employeesRoute: Router = Router();
@@ -16,6 +19,28 @@ employeesRoute.get(
 			});
 		} catch (err) {
 			return res.status(400).json({ message: (err as Error).message });
+		}
+	}
+);
+
+employeesRoute.get(
+	'/:code',
+	async (req: Request, res: Response): Promise<Response> => {
+		const { code } = req.params;
+
+		const employee: Employee | null = await findByCodeEmployee(
+			parseInt(code ?? '0')
+		);
+		try {
+			return employee
+				? res
+						.status(200)
+						.json({ message: 'Employee found successfully', data: employee })
+				: res
+						.status(400)
+						.json({ message: 'The employee does not exist in the database' });
+		} catch (err) {
+			return res.status(404).json({ message: (err as Error).message });
 		}
 	}
 );
