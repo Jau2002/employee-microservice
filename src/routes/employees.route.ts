1f4b85e9ecec9d1f87bbab5058f3c51ee66298a4
Jau2002 <idamendjaume@gmail.com>
2023-04-14 15:20:16 -0500

    feat: prceder controller route employe

---
+++ src/routes/employees.route.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const employeesRoute: Router = Router();
+
+export default employeesRoute;
--- src/routes/root.router.ts
+++ src/routes/root.router.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import employeesRoute from './employees.route';
 
 const rootRoute: Router = Router();
+
+rootRoute.use('/employees', employeesRoute);
 
 export default rootRoute;
