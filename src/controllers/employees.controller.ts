efbd9db5e59570ce0437ef0de61964d5601be5ae
Jau2002 <idamendjaume@gmail.com>
2023-04-17 16:38:19 -0500

    perf: reduce interator getter repository

--- src/controllers/employees.controller.ts
+++ src/controllers/employees.controller.ts
@@ -2,10 +2,10 @@ import type { Repository } from 'typeorm';
 import AppDataSource from '../config/db';
 import Employee from '../entity/Employee';
 
-export async function findAllEmployees(): Promise<Employee[]> {
-	const employeeRepository: Repository<Employee> =
+const employeeRepository: Repository<Employee> =
 		AppDataSource.getRepository(Employee);
 
+export async function findAllEmployees(): Promise<Employee[]> {
 	const allEmployees: Employee[] = await employeeRepository.find({
 		order: {
 			name: 'ASC',
@@ -17,9 +17,6 @@ export async function findAllEmployees(): Promise<Employee[]> {
 export async function findByCodeEmployee(
 	code: number
 ): Promise<Employee | null> {
-	const employeeRepository: Repository<Employee> =
-		AppDataSource.getRepository(Employee);
-
 	const allEmployees: Employee | null = await employeeRepository.findOne({
 		where: {
 			code,
@@ -32,9 +29,6 @@ export async function builderEmployee(
 	{ code, department, firstLastName, name, nif, secondLastName }: Employee,
 	EmployeeCode?: number
 ): Promise<Employee> {
-	const employeeRepository: Repository<Employee> =
-		AppDataSource.getRepository(Employee);
-
 	const createEmployee: Employee = await employeeRepository.save({
 		code: code ?? EmployeeCode,
 		department,
@@ -47,8 +41,5 @@ export async function builderEmployee(
 }
 
 export async function removeEmployee(employee: Employee): Promise<void> {
-	const employeeRepository: Repository<Employee> =
-		AppDataSource.getRepository(Employee);
-
 	await employeeRepository.remove(employee);
 }
