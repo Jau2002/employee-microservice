eef57c4c3ec29e371cb54ca3536a19582a813470
Jau2002 <idamendjaume@gmail.com>
2023-04-17 16:35:38 -0500

    feat: research departament unit later

--- src/controllers/departments.controller.ts
+++ src/controllers/departments.controller.ts
@@ -1,15 +1,26 @@
 import type { Repository } from 'typeorm';
-import AppDataSource from '../db/dbConfig';
+import AppDataSource from '../config/db';
 import Department from '../entity/Department';
 
-export async function listAllDepartments(): Promise<Department[]> {
-	const departmentRepository: Repository<Department> =
-		AppDataSource.getRepository(Department);
+const departmentRepository: Repository<Department> =
+	AppDataSource.getRepository(Department);
 
+export async function listAllDepartments(): Promise<Department[]> {
 	const allEmployees: Department[] = await departmentRepository.find({
 		order: {
 			name: 'ASC',
 		},
 	});
 	return allEmployees;
+}
+
+export async function getDepartmentsByCode(
+	code: number
+): Promise<Department | null> {
+	const findDepartment: Department | null = await departmentRepository.findOne({
+		where: {
+			code,
+		},
+	});
+	return findDepartment;
 }
--- src/routes/departments.route.ts
+++ src/routes/departments.route.ts
@@ -1,5 +1,8 @@
 import { Router, type Request, type Response } from 'express';
-import { listAllDepartments } from '../controllers/departments.controller';
+import {
+	getDepartmentsByCode,
+	listAllDepartments,
+} from '../controllers/departments.controller';
 import type Department from '../entity/Department';
 
 const departmentsRoute: Router = Router();
@@ -9,12 +12,35 @@ departmentsRoute.get(
 	async (_: Request, res: Response): Promise<Response> => {
 		const departments: Department[] = await listAllDepartments();
 		try {
-			return res
-				.status(200)
-				.json({
-					message: `${departments.length} Employees found`,
-					data: departments,
-				});
+			return res.status(200).json({
+				message: `${departments.length} Employees found`,
+				data: departments,
+			});
+		} catch (err) {
+			return res.status(400).json({ message: (err as Error).message });
+		}
+	}
+);
+
+departmentsRoute.get(
+	'/:code',
+	async (req: Request, res: Response): Promise<Response> => {
+		const { code } = req.params;
+
+		const foundDepartment: Department | null = await getDepartmentsByCode(
+			parseInt(code ?? '0')
+		);
+		try {
+			return foundDepartment
+				? res
+						.status(200)
+						.json({
+							message: 'Employee found successfully',
+							data: foundDepartment,
+						})
+				: res
+						.status(400)
+						.json({ message: 'The department does not exist in the database' });
 		} catch (err) {
 			return res.status(400).json({ message: (err as Error).message });
 		}
